51923543029b06177f4ff2c0ab59b5e0239fde1e
ichukkarlina193 <ichukkarlina@gmail.com>
2025-01-15 11:19:29 +0700

    Add files via upload

---
+++ tugasVariabel.js
@@ -0,0 +1,16 @@
+let namaDepan = "Ichuk maulidha karlina";
+let alamat = "Tambakroto,karang panas";
+let usia = "14tahun";
+let ttl = "Demak, 19 Maret 2010";
+let gender = "Perempuan";
+let agama = "Islam";
+let email = "ichukkarlina@gmail.com";
+let berat = "45kg";
+let tinggi = "162cm";
+let hobi = "menulis";
+let cita = "Pramugari";
+let pekerjaan = "Belajar";
+let sekolah = "SMKN 1 SAYUNG";
+let jurusan = "PPLG"
+
+document.write("nama saya adalah  "+ nama+",saya tinggal di "+ alamat+",pada tahun 2024 ini usia saya adalah "+usia+" tahun,saya lahir di"+ ttl+", jenis kelamin saya adalah "+ gender+", saya beragama "+ agama+",alamat email saya adalah "+ email+",saya memiliki berat badan "+ berat+"kg,dengan tinggi "+ tnggi+"cm, saya sangat senang "+ hobi+",karena saya memiliki cita-cita "+ cita+",saat ini saya adalah seorang "+ pekerjaan+",yang bersekolah di "+ sekolah+",pada jurusan "+ jurusan+"")
